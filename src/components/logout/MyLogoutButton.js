d22ea118ffe65d4caf9cbfa5c4119973b1a778f0
Truc Nguyen <trucnc1901@gmail.com>
2021-06-29 10:05:13 +0700

    update  logout button

--- src/components/logout/MyLogoutButton.js
+++ src/components/logout/MyLogoutButton.js
@@ -1,14 +1,27 @@
+import axios from 'axios';
+import StorageKeys from 'common/constant/storage-keys';
+import { getCookie } from 'components/common/Cookies';
 import React, { forwardRef } from 'react';
 import { useLogout } from 'react-admin';
-import { logout } from 'services/api/httpClient';
 import LogoutView from './LogoutView';
 
 const MyLogoutButton = forwardRef((props, ref) => {
   const logoutAdmin = useLogout();
   const handleLogout = async () => {
-    await logout.requestApi().then(() => {
-      logoutAdmin();
-    });
+    const logout = () => {
+      var requestOptions = {
+        headers: {
+          Authorization: `Bearer ${getCookie(StorageKeys.TOKEN)}`,
+        },
+      };
+      axios
+        .get('/api/v1/miniapps-console/users/me', requestOptions)
+        .then(function (response) {})
+        .catch(function (err) {
+          console.log(err.message);
+        });
+    };
+    logout(logoutAdmin());
   };
 
   return <LogoutView handleLogout={handleLogout} />;
